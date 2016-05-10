99b0708970e86eca4bb04ac5eb2f085fc4f0dcff
Kevin Hernon <khernon1@gmail.com>
2016-05-10 14:08:08 -0400

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,11 @@
 
 def using_for
 	checklist = 1..10
-	#your code here
+	for check in checklist
+		puts "Wingardium Leviosa"
+	end
 end
 
-
+using_for
+#* Fill out the content of the `using_for` method in the `for.rb` file so that it `puts`
+#{}"Wingardium Leviosa" ten (10) times, once for each item on our checklist.
